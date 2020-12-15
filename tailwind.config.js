c8e5e5abd57b3cc397d42e4f4fd3cfc2f66e28fb
Alexandre Di Guida <alexandre.diguida@gmail.com>
2020-12-15 14:00:23 +0000

    🔧 Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/pages/**/*.js', './src/components/**/*.js'], // remove unused styles in production
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        '-10': '-10',
+      },
+    },
   },
   variants: {
     extend: {},
