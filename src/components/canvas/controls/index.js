5af9e80bc2987e226ebb56efbd54a10150e7f395
Alexandre Di Guida <alexandre.diguida@gmail.com>
2021-01-08 13:34:08 +0000

    🚚 (hotfix/1.0.2): Rename some folders with lowercases

---
+++ src/components/canvas/controls/index.js
@@ -0,0 +1,22 @@
+import { useFrame, useThree } from 'react-three-fiber'
+
+import { OrbitControls } from '@react-three/drei'
+
+const Controls = ({ enableZoom = false, autoRotate = false }) => {
+  const { gl, camera } = useThree()
+
+  useFrame(() => {
+    camera.position.x = 5
+    camera.position.y = 0
+    camera.position.z = 0
+  })
+
+  return (
+    <OrbitControls
+      {...{ enableZoom, autoRotate }}
+      args={[camera, gl.domElement]}
+    />
+  )
+}
+
+export default Controls
